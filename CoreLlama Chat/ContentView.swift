5c4a50edc2bd96681dfcd69081ed2eea2aa20e95
helloNarehase <narehasedevel@gmail.com>
2024-10-30 06:07:30 +0900

    Initial Commit

---
+++ CoreLlama Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreLlama Chat
+//
+//  Created by 하늘 on 10/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreLlama Chat/CoreLlama_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLlama_ChatApp.swift
+//  CoreLlama Chat
+//
+//  Created by 하늘 on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreLlama_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
